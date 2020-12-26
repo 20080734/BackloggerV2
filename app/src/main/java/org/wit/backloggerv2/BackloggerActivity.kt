ce793e0791e435821239a95f9151c50cc1583d1b
Kevin Atkins <atkinsk021@gmail.com>
2020-12-26 15:09:32 +0000

    Basic hello world

---
+++ app/src/main/java/org/wit/backloggerv2/BackloggerActivity.kt
@@ -0,0 +1,14 @@
+package org.wit.backloggerv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+
+class BackloggerActivity : AppCompatActivity(), AnkoLogger {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_backlogger)
+        info("Backlogger activity started")
+    }
+}
